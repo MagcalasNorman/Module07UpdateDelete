644d3b411e139dab1b3571c5fd368d4c5f539489
MagcalasNorman <magcalas.normanmiguel@auf.edu.ph>
2024-10-30 13:09:58 +0800

    Add project files.

---
+++ Services/DatabaseConnectionService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07DataAccess.Services
+{
+    public class DatabaseConnectionService
+    {
+        private readonly string _connectiongString;
+
+        public DatabaseConnectionService()
+        {
+            _connectiongString = "Server=localhost; Database=testdb; User ID=testuser; Password=testuser";
+        }
+
+        public string GetConnectionString()
+        {
+            return _connectiongString;
+        }
+    }
+}
---
+++ ViewModel/PersonalViewModel.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module07DataAccess.Model;
+using Module07DataAccess.Services;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Net.WebSockets;
+
+
+namespace Module07DataAccess.ViewModel
+{
+    public class PersonalViewModel : INotifyPropertyChanged
+    {
+        private readonly PersonalService _personalService;
+        public ObservableCollection<Personal> PersonalList { get; set; }
+
+        private bool _isBusy;
+        public bool isBusy
+        {
+            get => _isBusy;
+            set { _isBusy = value; OnPropertyChanged(); }
+        }
+
+        private Personal _selectedPersonal;
+        public Personal SelectedPersonal
+        {
+            get => _selectedPersonal;
+            set
+            {
+                _selectedPersonal = value;
+                if (_selectedPersonal != null)
+                {
+                    NewPersonalName = _selectedPersonal.Name;
+                    NewPersonalGender = _selectedPersonal.Gender;
+                    NewPersonalContactNo = _selectedPersonal.ContactNo;
+                    IsPersonSelected = true;
+                }
+                else
+                {
+                    IsPersonSelected = false;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isPersonSelected;
+        public bool IsPersonSelected
+        {
+            get => _isPersonSelected;
+            set
+            {
+                _isPersonSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _statusMessage;
+        public string statusMessage
+        {
+            get => _statusMessage;
+            set { _statusMessage = value; OnPropertyChanged(); }
+        }
+
+        //New Personal Entry for Name, Gender, ContactNo
+
+        private string _newPersonalName;
+        public string NewPersonalName
+        {
+            get => _newPersonalName;
+            set
+            {
+                _newPersonalName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _newPersonalGender;
+        public string NewPersonalGender
+        {
+            get => _newPersonalGender;
+            set
+            {
+                _newPersonalGender = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _newPersonalContactNo;
+        public string NewPersonalContactNo
+        {
+            get => _newPersonalContactNo;
+            set
+            {
+                _newPersonalContactNo = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        public ICommand LoadDataCommand { get; }
+        public ICommand AddPersonalCommand { get; }
+        public ICommand SelectedPersonCommand { get; }
+        public ICommand DeletePersonCommand { get; }
+        public PersonalViewModel()
+        {
+            _personalService = new PersonalService();
+            PersonalList = new ObservableCollection<Personal>();
+            LoadDataCommand = new Command(async () => await LoadData());
+            AddPersonalCommand = new Command(async() => await AddPerson());
+            SelectedPersonCommand = new Command<Personal>(person => SelectedPersonal = person);
+            DeletePersonCommand = new Command(async() => await DeletePersonal(), () => SelectedPersonal != null);
+
+            LoadData();
+        }
+
+        public async Task LoadData()
+        {
+            if (isBusy) return;
+            isBusy = true;
+            statusMessage = "Loading personal data...";
+            try
+            {
+                var personals = await _personalService.GetAllPersonalsAsync();
+                PersonalList.Clear();
+                foreach (var personal in personals)
+                {
+                    PersonalList.Add(personal);
+                }
+                statusMessage = "Data Load Successfully";
+            }
+            catch (Exception ex)
+            {
+                statusMessage = $"Failed to Load data:{ex.Message}";
+            }
+            finally
+            {
+                isBusy = false;
+            }
+        }
+
+        private async Task AddPerson()
+        {
+            if (isBusy || string.IsNullOrWhiteSpace(_newPersonalName) || string.IsNullOrWhiteSpace(_newPersonalGender) || string.IsNullOrWhiteSpace(_newPersonalContactNo))
+            {
+                statusMessage = "Please fill in all the fields before adding";
+                return;
+            }
+            isBusy = true;
+            statusMessage = "Adding new person...";
+
+            try
+            {
+                var newPerson = new Personal
+                {
+                    Name = NewPersonalName,
+                    Gender = NewPersonalGender,
+                    ContactNo = NewPersonalContactNo
+                };
+                var isSuccess = await _personalService.AddPersonalAsync(newPerson);
+                if (isSuccess)
+                {
+                    NewPersonalName = string.Empty;
+                    NewPersonalGender = string.Empty;
+                    NewPersonalContactNo = string.Empty;
+                    statusMessage = "New Person added successfully!";
+                }
+                else 
+                {
+                    statusMessage = "Failed to add new person...";
+                }
+            }
+            catch (Exception ex)
+            {
+                statusMessage = $"Failed to add new person: {ex.Message}";
+            }
+
+            finally 
+            { 
+                isBusy = false;
+                await LoadData();
+            }
+        }
+
+        private async Task DeletePersonal()
+        {
+            if (SelectedPersonal == null) return;
+            var answer = await Application.Current.MainPage.DisplayAlert("Confirm Delete", $"Are you sure you want to delete {SelectedPersonal.Name}?", "Yes", "No");
+
+            if (!answer) return;
+            isBusy = true;
+            statusMessage = "Deleting person...";
+
+            try
+            {
+                var success = await _personalService.DeletePersonalAsync(SelectedPersonal.Id);
+                statusMessage = success ? "Person deleted successfully!" : "Failed to delete person";
+
+                if (success)
+                {
+                    PersonalList.Remove(SelectedPersonal);
+                    SelectedPersonal = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                statusMessage = $"Error deleting person: {ex.Message}";
+
+            }
+            finally 
+            {
+                isBusy = false;
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+
+    }
+}
